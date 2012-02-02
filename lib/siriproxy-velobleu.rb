8213d7b6b9cf097d8de63c6738bd998193068aef
David <da.laurent@laposte.net>
2012-02-02 01:07:55 +0100

    premier commit
    juste les outils de base pour les cartes

---
+++ lib/siriproxy-velobleu.rb
@@ -0,0 +1,153 @@
+# -*- encoding: utf-8 -*-
+require 'cora'
+require 'siri_objects'
+require 'eat'
+require 'nokogiri'
+require 'timeout'
+require 'json'
+require 'open-uri'
+require 'uri'
+require 'siren' #for sorting json hashes
+
+class SiriProxy::Plugin::Velobleu < SiriProxy::Plugin
+     
+    def initialize(config)
+      #if you have custom configuration options, process them here!
+    end
+
+    def userLongitude
+    end
+    def userLatitude
+    end
+
+	filter "SetRequestOrigin", direction: :from_iphone do |object|
+    	puts "[Info - User Location] lat: #{object["properties"]["latitude"]}, long: #{object["properties"]["longitude"]}"
+    	$userLongitude = object["properties"]["longitude"]
+    	$userLatitude = object["properties"]["latitude"]
+	end 
+
+# Where am i - shows map with current location
+listen_for /(Où suis-je|Où je suis|Localise-moi|Trouve moi|Trouve où je suis)/i do
+	if $userLongitude == NIL
+    	say "J'aime aussi le savoir, mais sans les données GPS, je suis juste un téléphone stupide."
+    else
+    	add_views = SiriAddViews.new
+    	add_views.make_root(last_ref_id)
+    	map_snippet = SiriMapItemSnippet.new(true)
+ 		siri_location = SiriLocation.new("", "Autour de vous", "", "", "", "", $userLatitude.to_f, $userLongitude.to_s) 
+	    map_snippet.items << SiriMapItem.new(label="Vous êtes ici", location=siri_location, detailType="BUSINESS_ITEM")
+	    print map_snippet.items
+	    utterance = SiriAssistantUtteranceView.new("Très bien, je vous localise.")
+		add_views.views << utterance
+    	add_views.views << map_snippet
+    
+   		#you can also do "send_object object, target: :guzzoni" in order to send an object to guzzoni
+    	send_object add_views #send_object takes a hash or a SiriObject object
+    end
+    request_completed #always complete your request! Otherwise the phone will "spin" at the user!
+  end
+
+# safes position in the file "locsave.txt"
+listen_for /(Position en mémoire|Enregistre la position|Sauvegarde la position|Enregistre ce lieu|Sauvegarde ce lieu|Enregistre cet endroit|Sauvegarde cet endroit)/i do   
+	lat = $userLatitude
+	lon = $userLongitude
+	if lat == nil
+		say "S'il vous plaît activer le service de suivi."
+	else
+		lats = lat.to_s
+		latt = lats.match(/[.]/)
+		latt = latt.post_match.strip.size
+		mystr = lat.to_s + "," + lon.to_s
+		aFile = File.new("plugins/siriproxy-velobleu/locsave.txt", "w")
+		aFile.write(mystr)
+		aFile.close
+		if latt < 13
+			say "Signal trop faible, sauvegardez la position" 
+		else
+			say "Emplacement enregistré, pour le chargez dites :'Charge la destination'", spoken: "Emplacement actuel enregistré"
+		end
+	end
+	#say "lat:" + $ortla.to_s + "  long:" + $ortlo.to_s , spoken: "" 
+	request_completed 
+end
+
+# loads position from a global variable
+listen_for /(Mes emplacements|Montre-moi la positon|Positions enregistrées|Affiche la position|Charger la position|Charge la destination)/i do 
+	aFile = File.new("plugins/siriproxy-velobleu/locsave.txt", "r")
+	str = aFile.gets.to_s
+	aFile.close
+	if str.match(/(,)/)
+		strloc = str.match(/(,)/)
+		lat = strloc.pre_match
+		lon = strloc.post_match
+	end
+	if lat.to_s == ""
+		say "Fichier vide, veuillez dire : 'Enregistrer la position'", spoken: "Veuillez enregistrer votre position."
+	else
+	lon1 = lon.to_f
+	lat1 = lat.to_f
+	lon2 = $userLongitude
+	lat2 = $userLatitude
+	if lon2 == NIL
+		say "S'il vous plaît activez les services de localisation."
+	else
+		haversine_distance( lat1, lon1, lat2, lon2 )
+		entf = @distances['km']
+		entf = (entf * 10**3).round.to_f / 10**3
+		if entf.to_s == "0.0"
+			say "Vous êtes arrivé à destination"
+			print entf
+		elsif entf > 0.0 and entf < 1.000
+			entf = (entf * 10**3).round.to_f / 10**3
+			ent = ent.to_f
+			ent = (entf * 1000)
+			ent = ent.to_s
+			ent = ent.match(/(.)/)
+			say "Distance de l'objectif: " + ent.to_s + " m", spoken: "Distance restante : " + ent.to_s + " Metres"
+	
+		else
+			say "Distance restante : " + entf.to_s + " km"
+		end
+	
+		add_views = SiriAddViews.new
+    	add_views.make_root(last_ref_id)
+    	map_snippet = SiriMapItemSnippet.new(true)
+    	siri_location = SiriLocation.new("Localisations stockées" , "Plan de votre destination", "Traduction 2", "Traduction 3", "durt", "wo", lat.to_f, lon.to_s) 
+    	map_snippet.items << SiriMapItem.new(label="Localisation de votre destination", location=siri_location, detailType="BUSINESS_ITEM")
+    	print map_snippet.items
+    	utterance = SiriAssistantUtteranceView.new("Hourra, je me suis trouvé !")
+    #add_views.views << utterance
+    	add_views.views << map_snippet
+    	send_object add_views #send_object takes a hash or a SiriObject object
+		end
+	end
+  request_completed
+end
+
+
+#    Thanks to http://www.esawdust.com/blog/businesscard/businesscard.html
+# for the distance calculation code
+def haversine_distance( lat1, lon1, lat2, lon2 )
+	self::class::const_set(:RAD_PER_DEG, 0.017453293)
+	self::class::const_set(:Rkm, 6371)              # radius in kilometers...some algorithms use 6367
+	self::class::const_set(:Rmeters, 6371000)    # radius in meters
+	@distances = Hash.new
+	dlon = lon2 - lon1
+	dlat = lat2 - lat1
+	dlon_rad = dlon * RAD_PER_DEG
+	dlat_rad = dlat * RAD_PER_DEG
+	lat1_rad = lat1 * RAD_PER_DEG
+	lon1_rad = lon1 * RAD_PER_DEG
+	lat2_rad = lat2 * RAD_PER_DEG
+	lon2_rad = lon2 * RAD_PER_DEG
+	a = (Math.sin(dlat_rad/2))**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * (Math.sin(dlon_rad/2))**2
+	c = 2 * Math.atan2( Math.sqrt(a), Math.sqrt(1-a))
+	dKm = Rkm * c             # delta in kilometers
+	dMeters = Rmeters * c     # delta in meters
+	@distances["km"] = dKm
+	@distances["m"] = dMeters
+end
+
+
+end
+ 
---
+++ siriproxy-velobleu.gemspec
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+$:.push File.expand_path("../lib", __FILE__)
+
+Gem::Specification.new do |s|
+  s.name        = "siriproxy-velobleu"
+  s.version     = "0.0.1" 
+  s.authors     = ["David LAURENT"]
+  s.email       = ["Twitter @dragouf"]
+  s.homepage    = ""
+  s.summary     = %q{An Example Siri Proxy Plugin}
+  s.description = %q{Devrait permettre d'obtenir des informations sur le velo bleu'. }
+
+  s.rubyforge_project = "siriproxy-eyelp"
+
+  s.files         = `git ls-files`.split("\n")
+  s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
+  s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
+  s.require_paths = ["lib"]
+
+  # specify any dependencies here; for example:
+  # s.add_development_dependency "rspec"
+  s.add_runtime_dependency "rest-client"
+  s.add_runtime_dependency "nokogiri"
+  s.add_runtime_dependency "eat"
+  s.add_runtime_dependency "httparty"
+  s.add_runtime_dependency "json"
+  s.add_runtime_dependency "siren"
+  #s.add_runtime_dependency "uri"
+end
